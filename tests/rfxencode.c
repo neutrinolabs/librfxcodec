ab9b65cee1d96eefe154de721fb375f7a4d3946a
Nexarian <cmp@pitstick.net>
2024-01-22 12:23:23 -0500

    Fixing build break.

--- tests/rfxencode.c
+++ tests/rfxencode.c
@@ -154,7 +154,8 @@ read_bitmap(char *file_name, int *width, int *height, int *bpp, char *bmp_data)
     return 0;
 }
 
-int out_params(void)
+static int
+out_params(void)
 {
     printf("rfxencode: a RemoteFX encoder testing program.\n");
     printf("  -i <in file name> bmp file\n");
@@ -165,7 +166,8 @@ int out_params(void)
     return 0;
 }
 
-int process(void)
+static int
+process(void)
 {
     char *out_data;
     char *bmp_data;
